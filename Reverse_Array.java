45c5c40516dbedd50416e69dd052aa792e9bf26c
Poonam soni <143243048+poonamsoni6264@users.noreply.github.com>
2024-09-16 10:34:32 +0530

    These files are  practice Question files.

---
+++ Reverse_Array.java
@@ -0,0 +1,16 @@
+package array_questions;
+
+public class Reverse_Array 
+{
+	public static void main(String[] args) 
+	{
+		int arr[] = {1,2,3,4,5};
+		
+		System.out.print("Reverse of Array : ");
+		for(int i=arr.length-1; i>=0; i--)
+		{
+			System.out.print(   arr[i] + " ");
+		}
+	}
+
+}
